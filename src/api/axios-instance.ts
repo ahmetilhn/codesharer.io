66d7901c7c27a47519c24f680911869f2b4f643e
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-12-18 20:36:26 +0300

    📦 axios added to project, axios interceptor, instance created

---
+++ src/api/axios-instance.ts
@@ -0,0 +1,9 @@
+import { setupInterceptors } from './axios-interceptors'
+import axios, { AxiosInstance } from 'axios'
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+setupInterceptors(axiosInstance)
---
+++ src/api/axios-interceptors.ts
@@ -0,0 +1,17 @@
+import { AxiosInstance, AxiosResponse, AxiosError } from 'axios'
+
+const resInterceptor = (res: AxiosResponse) => {
+  if (res.status >= 400 && res.status <= 599) {
+    throw new Error('Client or server error')
+  }
+  return res
+}
+
+const errHandler = (err: AxiosError) => {
+  console.error(err)
+}
+
+export const setupInterceptors = (axiosInstance: AxiosInstance): AxiosInstance => {
+  axiosInstance.interceptors.response.use(resInterceptor, errHandler)
+  return axiosInstance
+}

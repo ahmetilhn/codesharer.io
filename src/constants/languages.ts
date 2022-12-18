55b00a6e6588153f26019f50179bbf4febf35304
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-12-18 14:04:53 +0300

    dummy languages added

---
+++ src/constants/languages.ts
@@ -0,0 +1,14 @@
+export const languages = {
+  JAVASCRIPT: {
+    name: 'JavaScript',
+    extension: 'js',
+    value: 'javascript',
+    icon: '',
+  },
+  TYPESCRIPT: {
+    name: 'TypeScript',
+    extension: 'ts',
+    value: 'typescript',
+    icon: '',
+  },
+}

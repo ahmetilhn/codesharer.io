cb0980d01d7455e45d70c879f7311774779f12c8
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-12-20 00:31:59 +0300

    code updated func added

--- src/components/Editor/CodeMirror/index.tsx
+++ src/components/Editor/CodeMirror/index.tsx
@@ -4,9 +4,40 @@ import { javascript } from '@codemirror/lang-javascript'
 import { dracula } from '@uiw/codemirror-theme-dracula'
 import { StyledProvider } from './styles'
 import { editorStore } from '../../../store'
+import { useParams } from 'react-router-dom'
+import workspaceService from '../../../services/workspace.service'
+import { toast } from 'react-hot-toast'
+let fetchTimeout: ReturnType<typeof setTimeout>
 const CodeEditor: React.FC = () => {
+  const { id } = useParams()
   const { code } = editorStore((state) => state.workspace)
   const setCode = editorStore((state) => state.setCode)
+  const updateCode = async (code: string) => {
+    if (!id) return
+    type Payload = {
+      id: string
+      code: string
+    }
+    try {
+      const res = await workspaceService.updateWorkspace<Payload>({
+        id,
+        code,
+      })
+      if (res) {
+        toast.success('Code updated!')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const codeChangeHandler = (val: string) => {
+    clearTimeout(fetchTimeout)
+    setCode(val)
+    fetchTimeout = setTimeout(async () => {
+      await updateCode(val)
+    }, 4000)
+  }
   return (
     <StyledProvider>
       <CodeMirror
@@ -15,7 +46,7 @@ const CodeEditor: React.FC = () => {
         height='100%'
         theme={dracula}
         extensions={[javascript({ jsx: true })]}
-        onChange={(e) => setCode(e)}
+        onChange={(e) => codeChangeHandler(e)}
       />
     </StyledProvider>
   )
